938bea463f877abb62cd6bd43077a8bcc97ded8c
George <cgeoterd@gmail.com>
2021-11-23 23:24:39 +0200

    Change preview 1.0

--- EasyMV-example/example/assets/js/script.js
+++ EasyMV-example/example/assets/js/script.js
@@ -20,23 +20,34 @@ class EasyMV {
     setModelSrc(url) {
         this.model.src = url
     }
+    setCameraTarget(x,y,z) {
+        this.model.cameraTarget = `${x} ${y} ${z}`
+    }
+    setMaxCameraOrbit(x,y,z) {
+        this.model.maxCameraOrbit = `${x}, ${y}, ${z}`
+    }
+
 }
+
 
 let helmet = new EasyMV("test-model")
 helmet.model.addEventListener("load", () => {
     console.log(helmet)
     console.log(`helmet.setModelColor([0,0,0,1],4)`)
     console.log(`helmet.setModelCameraOrbit(0,60, 100)`)
-    helmetBtnSrc.addEventListener("click", (e) => {
-        helmet.setModelSrc("./assets/models/dji-phantom-4.glb")
+    changeBtn.addEventListener('click', () => {
+        helmet.setCameraTarget("auto 92m auto")
     })
-})
-let sneakers = new EasyMV("test-model_2")
-sneakers.model.addEventListener("load", () => {
-    console.log(sneakers)
-    console.log(`sneakers.setModelTexture("https://thumbs.dreamstime.com/z/white-grey-hexagon-background-texture-d-render-metal-illustration-82112026.jpg",0)`)
+    changeBtn.addEventListener('dblclick', () => {
+        helmet.setCameraTarget("auto 15m auto")
+    })
 
 })
+// let sneakers = new EasyMV("test-model_2")
+// sneakers.model.addEventListener("load", () => {
+//     console.log(sneakers)
+//     console.log(`sneakers.setModelTexture("https://thumbs.dreamstime.com/z/white-grey-hexagon-background-texture-d-render-metal-illustration-82112026.jpg",0)`)
+//
+// })
+let changeBtn = document.querySelector(".btn")
 
-
-let helmetBtnSrc = document.querySelector("#Helmet_src")
